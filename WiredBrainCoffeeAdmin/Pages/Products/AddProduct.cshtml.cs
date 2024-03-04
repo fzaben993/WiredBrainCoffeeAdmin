19c657ab378fe4d70d17a80ee50b74f8d5999d4f
Faisal Al-Zaben <fai20150710@std.psut.edu.jo>
2024-03-04 18:33:04 +0300

    Building the Add Product Form Using Tag Helpers

--- WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data;
 
 namespace WiredBrainCoffeeAdmin.Pages.Products
 {
     public class AddProductModel : PageModel
     {
+        public Product NewProduct { get; set; }
+
         public void OnGet()
         {
         }
