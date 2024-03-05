553d4da8b3192d072d47fa688ddcc11e431db4ba
Faisal Al-Zaben <fai20150710@std.psut.edu.jo>
2024-03-05 18:49:45 +0300

    Applying Essential Configurations

--- WiredBrainCoffeeAdmin/Program.cs
+++ WiredBrainCoffeeAdmin/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<WiredContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("WiredBrain")));
 
 var app = builder.Build();
 
