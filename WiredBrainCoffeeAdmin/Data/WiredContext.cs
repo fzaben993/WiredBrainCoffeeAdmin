858bd8ff722b55a2ba8f2a1d3a732c17601b44e5
Faisal Al-Zaben <fai20150710@std.psut.edu.jo>
2024-03-05 18:47:12 +0300

    Setting up the Entity Framework Classes

---
+++ WiredBrainCoffeeAdmin/Data/WiredContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WiredBrainCoffeeAdmin.Data;
+public class WiredContext : DbContext
+{
+    public WiredContext(DbContextOptions<WiredContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+}
