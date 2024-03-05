e14d73cece95ada4a20c3233da805abe07b798c2
Faisal Al-Zaben <fai20150710@std.psut.edu.jo>
2024-03-05 21:52:30 +0300

    Creating a Product List View Components

---
+++ WiredBrainCoffeeAdmin/Components/ProductListViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WiredBrainCoffeeAdmin.Services;
+
+namespace WiredBrainCoffeeAdmin.Components;
+public class ProductListViewComponent : ViewComponent
+{
+    private readonly IProductService _productService;
+
+    public ProductListViewComponent(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var products = await _productService.GetAll();
+
+        return View(products);
+    }
+}
